6c1213e3b753e19e4a85c913e2f5d9870c4ea095
0xABADCAFE <karlchurchill@gmail.com>
2023-10-10 12:17:41 +0100

    Adding zone debug skeleton

---
+++ ab3d2_source/c/zone_debug.c
@@ -0,0 +1,16 @@
+#include "system.h"
+#include "zone_debug.h"
+
+extern void ZDbg_Init(void)
+{
+
+}
+extern void ZDbg_Dump(void)
+{
+
+}
+
+extern void ZDbg_Done(void)
+{
+
+}
---
+++ ab3d2_source/c/zone_debug.h
@@ -0,0 +1,11 @@
+#ifndef ZONE_DEBUG_H
+#define ZONE_DEBUG_H
+
+/**
+ * These functions are to be called from Draw_Zone_Graph when the zone debug flag is set.
+ */
+extern void ZDbg_Init(void);
+extern void ZDbg_Dump(void);
+extern void ZDbg_Done(void);
+
+#endif // ZONE_DEBUG_H
