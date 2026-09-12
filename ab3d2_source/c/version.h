74fa4c5be3d97aa1bf7e79e7131812916887b291
0xABADCAFE <karlchurchill@gmail.com>
2026-09-13 00:24:51 +0100

    build bump.

--- ab3d2_source/c/version.h
+++ ab3d2_source/c/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #   define TKG_VERSION 1
 #   define TKG_REVISION 3
-#   define TKG_BUILD 6
-#   define TKG_HASH "010fc47"
+#   define TKG_BUILD 7
+#   define TKG_HASH "95fbfef"
 #endif
