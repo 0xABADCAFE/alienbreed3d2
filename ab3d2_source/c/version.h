f95a6491ccf985c47957d05d344a7b4e26f8a8d1
0xABADCAFE <karlchurchill@gmail.com>
2026-09-13 18:46:41 +0100

    Added outcode quadrant clipping code for map pulse. This ensures that only the necessary quadrants of a circle centred outside the viewport are processed.

--- ab3d2_source/c/version.h
+++ ab3d2_source/c/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #   define TKG_VERSION 1
 #   define TKG_REVISION 3
-#   define TKG_BUILD 7
-#   define TKG_HASH "95fbfef"
+#   define TKG_BUILD 8
+#   define TKG_HASH "96cdf1c"
 #endif
